1a684fcd30745a5940fc933bdce59cf5253aad95
Parker <park.thompson@gmail.com>
2021-08-02 13:15:53 -0500

    added test program with button listener

--- recordhowler.js
+++ recordhowler.js
@@ -1,2 +1,20 @@
 console.log(`Hello world`);
-console.log(`It's working!`);
+
+//Adds click listener to submit button
+document.getElementById("domain").addEventListener("click", runHowler);
+
+//Main program
+function runHowler() {
+    const domain = document.getElementById("domain").value;
+    console.log(domain);
+}
+
+//Fetches records from API
+function getRecords() {
+    fetch('', {
+        method: 'GET',
+        headers: {'Accept' : 'application/json'}
+    })
+    .then(response => response.json())
+    .then(data => document.getElementById("joke-text").innerHTML = `${data.x}`);
+}
