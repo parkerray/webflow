dcc5a9433383bd861577869f2d79ac8049ba7ee2
Parker <park.thompson@gmail.com>
2021-08-02 15:57:48 -0500

    Updated the API with input data

--- recordhowler.js
+++ recordhowler.js
@@ -4,14 +4,15 @@ console.log(`Hello world`);
 window.onload = function() {
     //Adds click listener to submit button
     const submitButton = document.querySelector("#submit");
+    const domainInput = document.querySelector("#domain");
     submitButton.addEventListener("click", runHowler);
 }
 
 //Main program
 function runHowler() {
-    const domain = document.getElementById("domain").value;
-    getRecords();
-    console.log(domain);
+    const domainInput = document.querySelector("#domain");
+    getRootRecords(domainInput);
+    getSubdomainRecords(domainInput);
 }
 
 //Gets the domain without http:// or https://
@@ -26,15 +27,35 @@ function getRootDomain(domain) {
     return rootDomain.join('');
 }
 
-//Fetches records from API
-function getRecords() {
-    fetch('https://www.whoisxmlapi.com/whoisserver/DNSService?apiKey=at_aXKafoG6V0tpe5ooMU0cxh7TZ0lNA&domainName=adalocado.com&type=A,AAAA,CNAME&outputFormat=JSON', {
+//Fetches A & AAAA records from API
+function getRootRecords(url) {
+    fetch(`https://www.whoisxmlapi.com/whoisserver/DNSService?apiKey=at_aXKafoG6V0tpe5ooMU0cxh7TZ0lNA&domainName=${url}&type=A,AAAA&outputFormat=JSON`, {
         method: 'GET',
     })
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => listRecords(data));
+}
+
+//Fetches CNAME records from API
+function getSubdomainRecords(url) {
+    fetch(`https://www.whoisxmlapi.com/whoisserver/DNSService?apiKey=at_aXKafoG6V0tpe5ooMU0cxh7TZ0lNA&domainName=${url}&type=CNAME&outputFormat=JSON`, {
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => listRecords(data));
 }
 
 console.log(getDomain(`https://www.adalocado.com/test`));
-console.log(getRootDomain(`https://www.adalocado.com/test`))
+console.log(getRootDomain(getDomain(url)));
+
+//Lists DNS records
+function listRecords() {
+    for (const key in list){
+        if(obj.hasOwnProperty(key)){
+        console.log(`${key} : ${list[key]}`)
+    }
+  }
+}
+
+listRecords();
 console.log(`the end`);
