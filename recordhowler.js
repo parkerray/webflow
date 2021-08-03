d5ce088c1c6da7c30d86c506a118f3f804cbb30d
Parker <park.thompson@gmail.com>
2021-08-02 19:39:49 -0500

    Updated a color

--- recordhowler.js
+++ recordhowler.js
@@ -138,7 +138,7 @@ function formatRecords() {
     records.forEach( record => {
         if (record.innerText.includes('99.83.190.102') || record.innerText.includes('75.2.70.75') || record.innerText.includes('proxy-ssl.webflow.com')) {
             record.style.backgroundColor = '#38d996';
-        } else record.style.backgroundColor = '#ff6382';
+        } else record.style.backgroundColor = '#ff8a8a';
     });
 }
 
