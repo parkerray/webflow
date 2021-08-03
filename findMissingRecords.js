8efaeca7abbf56b910746d035a461ea945d134de
Parker <park.thompson@gmail.com>
2021-08-02 22:00:36 -0500

    Dan's code is better

--- findMissingRecords.js
+++ findMissingRecords.js
@@ -1,37 +1,30 @@
 let neededRecords = [
-    {   'label': 'A',
-        'value':'99.83.190.102'
-    },
-    {   'label': 'A',
-        'value':'75.2.70.75'
-    },
+    {label: 'A', value: '99.83.190.102'},
+    {label: 'A', value: '75.2.70.75'},
     {
-        'label': 'CNAME',
-        'value': 'proxy-ssl.webflow.com'
-    }
-];
-
-let currentRecords = [
-    {   'label': 'A',
-        'value':'99.83.109.102'
+      label: 'CNAME',
+      value: 'proxy-ssl.webflow.com',
     },
+  ];
+  let currentRecords = [
+    {label: 'A', value: '99.83.109.102'},
     {
-        'label': 'CNAME',
-        'value': 'proxy-ssl.webflow.com'
-    }
-];
-
-function findMissingRecords(needed, current) {
+      label: 'CNAME',
+      value: 'proxy-ssl.webflow.com',
+    },
+  ];
+  function findMissingRecords(needList, currentList) {
     let result = [];
-    for (let i = 0; i < needed.length; i++) {
-        needed.findIndex(function (record) {
-            if (record.value === -1) {
-                result.push(current[i]);
-                console.log(current[i]);
-            };
-        })
-
-    } return result;
-}
-
-findMissingRecords(neededRecords, currentRecords);
+    for (let i = 0; i < needList.length; i++) {
+      const needed = needList[i];
+      const found = currentList.findIndex(
+        record => record.value === needed.value
+      );
+      if (found === -1) {
+        result.push(needed);
+      }
+    }
+    return result;
+  }
+  const missing = findMissingRecords(neededRecords, currentRecords);
+  console.log(missing);
